5b8eae9153215d7d74c3c908d34b52f58ed39245
Guns1301 <67137938+Guns1301@users.noreply.github.com>
2021-07-11 20:18:37 +0300

    Compiled successfully !!

---
+++ src/App.js
@@ -0,0 +1,66 @@
+import React, { Component } from "react";
+import Section from "./Section/Section.js";
+import Statistics from "./Statistics/Statistics.js";
+import FeedbackOptions from "./FeedbackOptions/FeedbackOptions.js";
+import Notification from "./Notification/Notification.js";
+
+import "./App.css";
+
+class App extends Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+
+  countFeedback = (event) => {
+    const { name } = event.currentTarget;
+
+    this.setState((prevState) => ({
+      [name]: prevState[name] + 1,
+    }));
+  };
+
+  countTotalFeedback = () => {
+    return this.state.good + this.state.neutral + this.state.bad;
+  };
+
+  countPositiveFeedbackPercentage = () => {
+    return this.countTotalFeedback()
+      ? Math.round((this.state.good / this.countTotalFeedback()) * 100)
+      : 0;
+  };
+  // у Класса есть метод рендер -
+  render() {
+    const { good, neutral, bad } = this.state;
+    const total = this.countTotalFeedback();
+    const positivePercentage = this.countPositiveFeedbackPercentage();
+
+    return (
+      //тут и будем рендерить разметку
+      <div className="wrapper">
+        <Section title="Please leave your feedback about Espresso Cafe">
+          <FeedbackOptions
+            options={["good", "neutral", "bad"]}
+            onLeaveFeedback={this.countFeedback}
+          />
+        </Section>
+        <Section title="Statistics">
+          {total ? (
+            <Statistics
+              good={good}
+              neutral={neutral}
+              bad={bad}
+              total={total}
+              positivePercentage={positivePercentage}
+            />
+          ) : (
+            <Notification />
+          )}
+        </Section>
+      </div>
+    );
+  }
+}
+
+export default App;
